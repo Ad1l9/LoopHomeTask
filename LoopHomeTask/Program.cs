35ebdd8f59ed25a085e01d4a5dbde62bb56f8e1a
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-28 00:27:49 +0400

    Add project files.

---
+++ LoopHomeTask/Program.cs
@@ -0,0 +1,29 @@
+﻿// Task1
+//1.Verilmish ededin 2 quvveti olub olmadigini tapan alqoritm
+//(Meselen 8 , 4, 16 ve s. 2-nin quvvetidir amma 12  2-nin quvveti deyil) 
+
+
+int num = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (num % 2 == 0 && num / 2 >= 2)num /= 2;
+    
+    
+    else if(num % 2 == 1)
+    {
+        Console.WriteLine("2 quvveti deyil");
+        break;
+    }
+
+
+    if (num / 2 == 1)
+    {
+        Console.WriteLine("2 quvvetidir");
+        break;
+    }
+    
+}
+
+
+
