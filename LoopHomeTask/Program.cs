175269d83e2e366be3bc10636bbab258490498c7
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-28 01:25:42 +0400

    Task2 added

--- LoopHomeTask/Program.cs
+++ LoopHomeTask/Program.cs
@@ -3,27 +3,62 @@
 //(Meselen 8 , 4, 16 ve s. 2-nin quvvetidir amma 12  2-nin quvveti deyil) 
 
 
-int num = int.Parse(Console.ReadLine());
+//int num = int.Parse(Console.ReadLine());
 
-while (true)
+//while (true)
+//{
+//    if (num % 2 == 0 && num / 2 >= 2)num /= 2;
+
+
+//    else if(num % 2 == 1)
+//    {
+//        Console.WriteLine("2 quvveti deyil");
+//        break;
+//    }
+
+
+//    if (num / 2 == 1)
+//    {
+//        Console.WriteLine("2 quvvetidir");
+//        break;
+
+//    }
+
+//}
+
+
+
+
+
+
+
+
+//Task2
+//1-dən N-ə qədər ədədlər arasında neçə digit rəqəmi var?
+
+using System;
+
+int N=int.Parse(Console.ReadLine());
+int digit=int.Parse(Console.ReadLine());
+int count = 1;
+int num;
+
+for(int i = 10; i <= N; i++)
 {
-    if (num % 2 == 0 && num / 2 >= 2)num /= 2;
-    
-    
-    else if(num % 2 == 1)
+    num = i;
+    while (num / 10 > 0)
     {
-        Console.WriteLine("2 quvveti deyil");
-        break;
-    }
+        if (num% 10 == digit)
+        {
+            count++;
+        }
+        num = (num - num % 10) / 10;
 
-
-    if (num / 2 == 1)
-    {
-        Console.WriteLine("2 quvvetidir");
-        break;
     }
-    
+    if (num == digit) count++;
 }
+Console.WriteLine(count);
+
 
 
 
